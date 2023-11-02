14f3ba7672bafa41fbe980fb484caf99d86062bd
Alex Pasichnichenko <opasichnichenko@digitalsamba.com>
2023-11-02 11:06:35 +0200

    Initial Commit

---
+++ SambaEmbeddedStoryboardDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SambaEmbeddedStoryboardDemo
+//
+//  Created by alex on 02.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
